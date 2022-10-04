4a526fde6ec8ffb3aae123e0d4b28e1142617055
Abdulrahmanh95 <abd.alrahmanh.1995@gmail.com>
2022-10-04 12:17:35 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
+function App() {
+    return (
+        <main className="h-full">
+            <Outlet />
+        </main>
+    );
+}
+
+export default App
